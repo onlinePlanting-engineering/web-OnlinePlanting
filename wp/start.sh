352b46f77996318433d015b2a2420971260207bb
Frankin.Fu <fuzhiyong@onlineplanting.com>
2017-09-16 16:07:45 +0800

    1. mapping wordpress docker /app directory to wd. 2. remove wp/wp-config.php from git cache.

--- wp/start.sh
+++ wp/start.sh
@@ -7,12 +7,13 @@ DB_PORT=${DB_PORT:=3306}
 DB_USER=${DB_USER:=admin}
 DB_PASS=${DB_PASS:=planting}
 DB_NAME=${DB_NAME:=planting2}
+CUR_DIR=$(cd `dirname $0`; pwd)
 
 #docker build -t planting/wordpress:4.8 .
 
 docker run -d -p 8080:80 --name wordpress \
 	--link dbserver:db \
-	-v /app \
+	-v ${CUR_DIR}:/app \
 	-e WORDPRESS_DB_HOST=${DB_HOST}:${DB_PORT} \
 	-e WORDPRESS_DB_USER=${DB_USER} \
 	-e WORDPRESS_DB_PASSWORD=${DB_PASS} \
